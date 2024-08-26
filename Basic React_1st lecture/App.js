fc8112069f52546b91690f38ec0bfaab1d6e3654
Aman Kumar Shrivastava <amankumarshrivastava835@gmail.com>
2024-08-27 00:16:45 +0530

    update

--- Basic React_1st lecture/App.js
+++ Basic React_1st lecture/App.js
@@ -1,3 +1,15 @@
-const element=React.createElement("h1",{},"Hello World");
+const element=React.createElement(
+    "div",{id:"parent"},[
+        React.createElement("div",{id:"child1"},[
+            React.createElement("h1",{},"I am h1 tag"),
+            React.createElement("h2",{},"I am h2 tag")
+        ]
+    ),
+    React.createElement("div",{id:"child2"},[
+        React.createElement("h1",{},"I am h1 tag"),
+        React.createElement("h2",{},"I am h2 tag")
+    ])
+]
+);
 const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(element);
