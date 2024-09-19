a54b7cf7ff6f003c30c077c98d582d83a05884b6
Aman Kumar Shrivastava <amankumarshrivastava835@gmail.com>
2024-09-20 00:37:44 +0530

    update foodvila

--- foodvila/src/components/Body.js
+++ foodvila/src/components/Body.js
@@ -1,11 +1,22 @@
- import RestrauntCard from './RestrauntCard';
+import { useState } from 'react';
+import RestrauntCard from './RestrauntCard';
 
 
 const Body=()=>{
+    const [searchInput, setSearchInput] = useState("KFC");
+
+
+    
     return(
+        <>
+        <div className='search-container'>
+            <input type='text' className='search-input'placeholder='Search' value={searchInput} onChange={(e)=>setSearchInput(e.target.value)} />
+            <button className='search-btn'>Search</button>
+        </div>
       <div>
         <RestrauntCard/>
       </div>
+      </>
     );
   }
 
